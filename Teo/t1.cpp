f3e232f16fc65074a0d5cc1191dba0fda3c3ca96
goncalo2k <goncalo.miranda.cplx@gmail.com>
2022-04-08 08:53:29 +0100

    Theorical ex's

--- Teo/t1.cpp
+++ Teo/t1.cpp
@@ -2,9 +2,21 @@
 
 using namespace std;
 
-    
+namespace a {
+  int f(int x, int y = 3) { return x + y; }
+  double f(int a, int b) {return a+b;}
+}
+namespace b {
+  int f(int x=1) { return x + 1; }
+  double f(int x, double y) { return x + y; }
+  //double f(int a, int b=1) {return a+b;}
+}
+int f(int x) {
+  return x;
+}
 
-int main()
+
+int main() 
 {
     return 0;
 }
